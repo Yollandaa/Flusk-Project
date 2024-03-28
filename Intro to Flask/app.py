68ae9fdfff1ea7623c9f3a6935606206b2512a4a
Yollandaa <yolanda.dastile@gmail.com>
2024-03-28 15:04:13 +0200

    Flask Forms and Validations

--- Intro to Flask/app.py
+++ Intro to Flask/app.py
@@ -8,21 +8,15 @@ from pprint import pprint
 import uuid
 
 load_dotenv()  # load -> temporary as env
-
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get(
+    "FORM_SECRET_KEY"
+)  # token, different for each user -> should be hidden
 # mssql+pyodbc://<username>:<password>@<dsn_name>?driver=<driver_name>
 
 connection_String = os.environ.get("AZURE_DATABASE_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = connection_String
 db = SQLAlchemy(app)
-
-try:
-    with app.app_context():
-        # Use text() to explicitly declare your SQL command
-        result = db.session.execute(text("SELECT 1")).fetchall()
-        print("Connection successful:", result)
-except Exception as e:
-    print("Error connecting to the database:", e)
 
 # Model (SQLAlchemy) == Schema
 
@@ -64,8 +58,8 @@ class Movie(db.Model):
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, default=lambda: str(uuid.uuid4()))
-    username = db.Column(db.String(100))
-    password = db.Column(db.String(100))
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
 
     def to_dict(self):
         return {
@@ -74,6 +68,15 @@ class User(db.Model):
             "password": self.password,
         }
 
+
+try:
+    with app.app_context():
+        # Use text() to explicitly declare your SQL command
+        result = db.session.execute(text("SELECT 1")).fetchall()
+        print("Connection successful:", result)
+        # db.create_all()  # This creates the table if it diesn't exist in the mssm database
+except Exception as e:
+    print("Error connecting to the database:", e)
 
 # local
 # /dashboard
--- Intro to Flask/users_bp.py
+++ Intro to Flask/users_bp.py
@@ -1,43 +1,125 @@
 from flask import Blueprint, render_template, request
 from app import User, db
+from wtforms import PasswordField, StringField, SubmitField, ValidationError
+from wtforms.validators import InputRequired, Length
+from flask_wtf import FlaskForm
+
 
 users_bp = Blueprint("users", __name__)
 
 
-@users_bp.route("/login", methods=["GET"])
-def login_page():
-    return render_template("forms.html", id="login")
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(min=6)])
+    password = PasswordField(
+        "Password", validators=[InputRequired(), Length(min=8, max=12)]
+    )
+    submit = SubmitField("Sign up")
+
+    # This will be called automatically when the form is submitted
+    def validate_username(self, field):
+        # Informing WTF there's an error
+        user = User.query.filter_by(username=field.data).first()
+        if user:
+            raise ValidationError("Username already exists")
 
 
-@users_bp.route("/signup", methods=["GET"])
-def signup_page():
-    return render_template("forms.html", id="sign-up")
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(min=6)])
+    password = PasswordField(
+        "Password", validators=[InputRequired(), Length(min=8, max=12)]
+    )
+    submit = SubmitField("Login")
+
+    def validate_username(self, field):
+        user = User.query.filter_by(username=field.data).first()
+        if user is None:
+            raise ValidationError("username does not exist")
+
+    # Validate for login form
+    def validate_password(self, field):
+        user = User.query.filter_by(username=self.username.data).first()
+        if user:
+            user_db_data = user.to_dict()
+            form_password = field.data
+            if user_db_data["password"] != form_password:
+                print(user_db_data["password"])
+                raise ValidationError("Incorrect password")
 
 
-@users_bp.route("/logged-in", methods=["POST"])
-def validate_login():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    # Get details from User where usernames are equal
-    user = User.query.filter_by(username=username).first()
-    if user:
-        if user.password == password:
-            return f"<h1>Hello, {username}</h1>"
-        else:
-            return f"<h1>Invalid Password</h1>"
-    else:
-        return f"<h1>Username not found</h1>"
+@users_bp.route("/register", methods=["POST", "GET"])
+def register():
+    # GET and POST
+    form = RegistrationForm()
+    # ON POST
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        new_user = User(username=username, password=password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return f"<h1> {username} Registration successful </h1>", 201
+        except Exception as e:
+            db.session.rollback()
+            return f"<h1> Registration Failed </h1>", 404
+
+    # ONLY GET
+    return render_template("register.html", form=form)
 
 
-@users_bp.route("/signed-in", methods=["POST"])
-def sign_in():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    new_user = User(username=username, password=password)
-    try:
-        db.session.add(new_user)
-        db.session.commit()
-        return "<h1>Signed In Successfully</h1>"
-    except Exception as e:
-        db.session.rollback()
-        return f"<h1>Failed to register as a user{str(e)}</h1>"
+# GET - Issue a token
+# POST - Verify token
+
+
+@users_bp.route("/login", methods=["POST", "GET"])
+def login():
+    # GET and POST
+    form = LoginForm()
+    # ON POST
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        return "<h1>Successfully logged in</h1>"
+
+    return render_template("login.html", form=form)
+
+
+# ---------------------------- Using HTML ----------------------------------
+# @users_bp.route("/login", methods=["GET"])
+# def login_page():
+#     return render_template("forms.html", id="login")
+
+
+# @users_bp.route("/signup", methods=["GET"])
+# def signup_page():
+#     return render_template("forms.html", id="sign-up")
+
+
+# @users_bp.route("/logged-in", methods=["POST"])
+# def validate_login():
+#     username = request.form.get("username")
+#     password = request.form.get("password")
+#     # Get details from User where usernames are equal
+#     user = User.query.filter_by(username=username).first()
+#     if user:
+#         if user.password == password:
+#             return f"<h1>Hello, {username}</h1>"
+#         else:
+#             return f"<h1>Invalid Password</h1>"
+#     else:
+#         return f"<h1>Username not found</h1>"
+
+
+# @users_bp.route("/signed-in", methods=["POST"])
+# def sign_in():
+#     username = request.form.get("username")
+#     password = request.form.get("password")
+#     new_user = User(username=username, password=password)
+#     try:
+#         db.session.add(new_user)
+#         db.session.commit()
+#         return "<h1>Signed In Successfully</h1>"
+#     except Exception as e:
+#         db.session.rollback()
+#         return f"<h1>Failed to register as a user{str(e)}</h1>"
